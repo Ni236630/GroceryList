b5761fdb04ecbe1fe3c4c99b368cb049abc15dbb
Ni236630 <nicholas.douso@gmail.com>
2021-01-29 14:53:45 -0600

    added navbar;small style added

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -4,10 +4,21 @@ import "./Navbar.css"
 
 export const Navbar = () => {
     return (
+      <>
+      
+        <div className="navbar__container menu">
         <ul className="navbar">
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/">Home</Link>
             </li>
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/recipes">Recipes</Link>
+            </li>
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/grocerylists">Grocery Lists</Link>
+            </li>
         </ul>
+        </div>
+        </>
     )
 }
