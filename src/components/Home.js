08bc1e58495eb45a49fe2932e48059fb0bdfd8e9
Ni236630 <nicholas.douso@gmail.com>
2021-02-01 13:20:18 -0600

    fixed menu close on click outside

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,23 @@
-import React, { useState }  from "react";
+import React, { useState, useRef }  from "react";
 import { Navbar } from './nav/Navbar'
 import { ApplicationViews } from "./ApplicationViews";
+import { useOnClickOutside } from './CloseMenu'
 import './Home.css'
 
 
 
 export const Home = () =>{
-  
+  const node = useRef()
+  useOnClickOutside(node, ()=> setOpen(false))
    const [ open, setOpen] = useState(false)
   
-  return  <div>
+  return  <div ref={node}>
     <div open={open} onClick={()=> setOpen(!open)} className="menu__button--container">
       <div className='menu__button'></div>
       <div className='menu__button'></div>
       <div className='menu__button'></div>
     </div>
-      <Navbar key={1} open={open} setOpen={setOpen} />
+      <Navbar open={open} setOpen={setOpen} />
       <ApplicationViews open={open} setOpen={setOpen}/>
     </div>
   
