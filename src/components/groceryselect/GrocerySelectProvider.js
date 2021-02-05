a24359edeb450381e552d83e7b13c06e6910eb4c
Ni236630 <nicholas.douso@gmail.com>
2021-02-05 12:09:49 -0600

    added add list function

--- src/components/groceryselect/GrocerySelectProvider.js
+++ src/components/groceryselect/GrocerySelectProvider.js
@@ -7,14 +7,25 @@ export const GrocerySelectProvider = (props) => {
   const [ grocerySelect, setGrocerySelect ] = useState([])
   
   const getGrocerySelect = () => {
-    return fetch('http://localhost:8088/')
+    return fetch('http://localhost:8088/groceryLists')
       .then(res=>res.json())
       .then(setGrocerySelect)
   }
   
+  const addGrocerySelect = (grocerylistObj) => {
+    return fetch('http://localhost:8088/groceryLists',{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify(grocerylistObj)
+    })
+      .then(getGrocerySelect)
+  }
+  
   return(
    <GrocerySelectContext.Provider value={{
-     getGrocerySelect, grocerySelect
+     getGrocerySelect, grocerySelect, addGrocerySelect
    }}>
      {props.children}
    </GrocerySelectContext.Provider>
